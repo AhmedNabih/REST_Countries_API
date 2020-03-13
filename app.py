bffab19190b675fcfc74b7e1166b00302115ab48
Ahmed Nabih <ahmed.nabih01@gmail.com>
2020-03-13 04:00:28 +0200

    API Response test is working

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+import flask
+import requests
+import json
 
-app = Flask(__name__)
+# Global Variables
+from flask import jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+api_url_base = 'https://restcountries.eu/rest/v2/'
 
 
-@app.route('/')
-def index():
-    return "ahmed"
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/', methods=['GET'])
+def home():
+    info = get_country_info()
+    if info is not None:
+        return jsonify(info)
+    else:
+        return page_not_found
+
+
+def get_country_info():
+    api_url = api_url_base + 'name/egypt'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        return json.loads(response.content)
+    else:
+        return None
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()

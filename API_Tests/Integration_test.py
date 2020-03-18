167205a11353948a0eb62b53a38102b35d197790
Karememad21 <49999717+Karememad21@users.noreply.github.com>
2020-03-18 20:27:35 +0200

    integration test

--- API_Tests/Integration_test.py
+++ API_Tests/Integration_test.py
@@ -1,15 +1,52 @@
 import json
 import unittest
 
+from flask import jsonify, Flask
 from mock import patch, Mock
 
 from API.Base_API import BaseAPI
+from API.offline_Data import OfflineData
 
 
 class Integration(unittest.TestCase):
 
-    def setUp(self):
-        self.baseAPI = BaseAPI()
+    def test_RunIntegration1(self):
+        app = Flask(__name__)
+        with app.app_context():
+            baseAPI = BaseAPI()
+            countryInfo = baseAPI.get_country_info(str("egypt"), "alpha3Code")  # EGY
+            if countryInfo is None:
+                countryInfo = "Not Found Or Invalid Input"
 
-    def RunIntegration(self):
-        pass
+            output = jsonify(countryInfo)
+            self.assertEqual(str(jsonify("EGY")), str(output))
+
+    def test_RunIntegration2(self):
+        app = Flask(__name__)
+        with app.app_context():
+            FilePath = "TestData/testData.json"
+            offData = OfflineData(FilePath)
+            offData.OpenFile()
+            offData.LoadFile()
+            offData.CloseFile()
+            countryInfo = offData.GetData("egypt", "alpha3Code")  # EGY
+            if countryInfo is None:
+                countryInfo = "Not Found Or Invalid Input"
+
+            output = jsonify(countryInfo)
+            self.assertEqual(str(jsonify("EGY")), str(output))
+
+    def test_RunIntegration3(self):
+        app = Flask(__name__)
+        with app.app_context():
+            FilePath = "TestData/testData.json"
+            offData = OfflineData(FilePath)
+            offData.OpenFile()
+            offData.LoadFile()
+            offData.CloseFile()
+            countryInfo = offData.GetData("egypt", "s")  # EGY
+            if countryInfo is None:
+                countryInfo = "Not Found Or Invalid Input"
+
+            output = jsonify(countryInfo)
+            self.assertEqual(str(jsonify("Not Found Or Invalid Input")), str(output))

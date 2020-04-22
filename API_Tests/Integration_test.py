7d66f4f84b95f8fd49c23f6d5046e7712fca6f6a
Ahmed Nabih <ahmed.nabih01@gmail.com>
2020-04-22 17:31:10 +0200

    fix integration tests

--- API_Tests/Integration_test.py
+++ API_Tests/Integration_test.py
@@ -3,9 +3,7 @@ import unittest
 
 from flask import jsonify, Flask
 from mock import patch, Mock
-from API.offline_Data import OfflineData
 from API.Base_API import BaseAPI
-from API.offline_Data import OfflineData
 
 
 class Integration(unittest.TestCase):
@@ -21,33 +19,3 @@ class Integration(unittest.TestCase):
                 countryInfo = "Not Found Or Invalid Input"
 
             self.assertEqual(str("EGY"), str(countryInfo))
-
-    def test_RunIntegration2(self):
-        app = Flask(__name__)
-        with app.app_context():
-            FilePath = "TestData/testData.json"
-            offData = OfflineData(FilePath)
-            offData.OpenFile()
-            offData.LoadFile()
-            offData.CloseFile()
-            countryInfo = offData.GetData("egypt", "alpha3Code")  # EGY
-            if countryInfo is None:
-                countryInfo = "Not Found Or Invalid Input"
-
-            output = jsonify(countryInfo)
-            self.assertEqual(str(jsonify("EGY")), str(output))
-
-    def test_RunIntegration3(self):
-        app = Flask(__name__)
-        with app.app_context():
-            FilePath = "TestData/testData.json"
-            offData = OfflineData(FilePath)
-            offData.OpenFile()
-            offData.LoadFile()
-            offData.CloseFile()
-            countryInfo = offData.GetData("egypt", "s")  # EGY
-            if countryInfo is None:
-                countryInfo = "Not Found Or Invalid Input"
-
-            output = jsonify(countryInfo)
-            self.assertEqual(str(jsonify("Not Found Or Invalid Input")), str(output))

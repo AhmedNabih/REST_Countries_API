58dee352d0fde0ef61eea776f963087f71e8f634
Ahmed Nabih <ahmed.nabih01@gmail.com>
2020-04-22 17:28:07 +0200

    remove offline data

--- API/app.py
+++ API/app.py
@@ -2,7 +2,6 @@ from flask import render_template, jsonify, Flask
 
 from API.Base_API import BaseAPI
 from API.InternetConnection import Connection
-from API.offline_Data import OfflineData
 
 # Global variable
 INTERNETCONNECTION = False
@@ -12,15 +11,8 @@ app = Flask(__name__)
 
 @app.route("/<countryName>/<category>/")
 def outputPage(countryName, category):
-    if INTERNETCONNECTION:
-        baseAPI = BaseAPI()
-        countryInfo = baseAPI.get_country_info(str(countryName), category)
-    else:
-        offData = OfflineData(FilePath)
-        offData.OpenFile()
-        offData.LoadFile()
-        offData.CloseFile()
-        countryInfo = offData.GetData(countryName, category)
+    baseAPI = BaseAPI()
+    countryInfo = baseAPI.get_country_info(str(countryName), category)
 
     if countryInfo is None:
         countryInfo = "Not Found Or Invalid Input"
